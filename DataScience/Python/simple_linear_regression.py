fe1effe1b83b23c88c00f696cd5e6eea6cf0bdc1
raulfco10 <raulfco10@users.noreply.github.com>
2019-09-15 23:10:31 -0500

    Update simple_linear_regression.py

--- DataScience/Python/simple_linear_regression.py
+++ DataScience/Python/simple_linear_regression.py
@@ -8,15 +8,39 @@ import pandas as pd
 
 #Importar el dataset
 dataset = pd.read_csv('Salary_Data.csv')
-X=dataset.iloc[:,:-1].values
-y=dataset.iloc[:,3].values
+X=dataset.iloc[:,:-1].values #Todas las columnas excepto la ultima
+y=dataset.iloc[:,-1].values
 
 #Dividir el dataset en entrenamiento y testing
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0) #Por la cantidad tan pequena de la muestra se usara un 33% de texting
 
-#Escalado de Variables
-from sklearn.preprocessing import StandardScaler
-sc_X = StandardScaler()
-X_train = sc_X.fit_transform(X_train)
-X_test = sc_X.transform(X_test)
+#Escalado de Variables #Con regresion lineal simple no es necesario el escalado
+#from sklearn.preprocessing import StandardScaler
+#sc_X = StandardScaler()
+#X_train = sc_X.fit_transform(X_train)
+#X_test = sc_X.transform(X_test)
+
+#Crear modelo de regresion lineal simple con el conjunto de entrenamiento
+from sklearn.linear_model import LinearRegression
+regression = LinearRegression()
+regression.fit(X_train, y_train)
+
+#Predecir el conjunto de test
+y_pred = regression.predict(X_test)
+
+#Visualizar los resultados de entrenamiento
+plt.scatter(X_train, y_train, color = "red")
+plt.plot(X_train, regression.predict(X_train), color = "blue")
+plt.title("Sueldo vs Anios de experiencia (Conjunto de entrenamiento)")
+plt.xlabel("Anios de experiencia")
+plt.ylabel("Sueldo (USD)")
+plt.show()
+
+#Visualizar los resultados de test
+plt.scatter(X_test, y_test, color = "red")
+plt.plot(X_train, regression.predict(X_train), color = "blue")
+plt.title("Sueldo vs Anios de experiencia (Conjunto de testing)")
+plt.xlabel("Anios de experiencia")
+plt.ylabel("Sueldo (USD)")
+plt.show()
